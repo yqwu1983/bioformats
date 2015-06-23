26420dc0ed8305b020d48d78b29a0da59f8bbc2d
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-06-23 17:29:46 +0300

    Package exception class added

---
+++ bioformats/exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 by Gaik Tamazian
+# gaik (dot) tamazian (at) gmail (dot) com
+
+
+class BioformatsError(Exception):
+    """
+    The class describes a basic error that may occur in routines of
+    the bioformats package.
+    """
+    pass
