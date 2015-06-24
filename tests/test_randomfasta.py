d6ef56c9ecb989298b0f073a7c984bf1219bea30
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-06-24 14:05:10 +0300

    Test suite for randomfasta tool added

---
+++ tests/test_randomfasta.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 by Gaik Tamazian
+# gaik (dot) tamazian (at) gmail (dot) com
+
+import os
+import sys
+import tempfile
+import unittest
+import bioformats.randomfasta
+
+
+class TestRandomFasta(unittest.TestCase):
+    def setUp(self):
+        self.__output_file = tempfile.NamedTemporaryFile().name
+
+    def test_randomfasta(self):
+        """
+        Check if a random FASTA file is created.
+        """
+        sys.argv = ['', '10', '10', self.__output_file]
+        bioformats.randomfasta.main()
+        os.unlink(self.__output_file)
