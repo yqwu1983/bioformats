058c1826904eaecb5789fe9c4cdcccce750a5422
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-09-09 23:51:27 +0300

    Version raised

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bioformats',
-      version='0.1.5.post1',
+      version='0.1.5.post2',
       description='Classes to handle bioinformatics data',
 
       author='Gaik Tamazian',
