d18afbbfeb1201f58cefb565f12f177f80ab99d4
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-09-17 19:45:35 +0300

    DNA HGVS parsing function added

--- bioformats/snpeff.py
+++ bioformats/snpeff.py
@@ -20,6 +20,37 @@ snpeff_columns = ('allele', 'annotation', 'putative_impact',
 
 Record = namedtuple('Record', snpeff_columns)
 
+HgvsRecord = namedtuple('HgvsRecord', ('pos', 'ref', 'alt'))
+
+
+def parse_hgvs_dna(x):
+    """
+    Parse a HGVS notation record.
+
+    :param x: a DNA variant in the HGVS notation
+    :type x: str
+    :return: a parsed HGVS record
+    :rtype: HgvsRecord
+    """
+    if not x.startswith('c.'):
+        logger.error('incorrect HGVS DNA notation %s', x)
+        raise SnpEffError
+    x = x[2:]
+    x_parts = x.split('>', 1)
+    if len(x_parts) < 2:
+        logger.error('incorrect HGVS DNA notation %s', x)
+        raise SnpEffError
+    pos = x_parts[0][:-1]
+    try:
+        pos = int(pos)
+    except ValueError:
+        logger.error('incorrect numeric value %s in HGVS notation',
+                     pos)
+        raise SnpEffError
+    ref, alt = x_parts[0][-1], x_parts[1]
+
+    return HgvsRecord(pos=pos, ref=ref, alt=alt)
+
 
 def parse_snpeff_annotation(annotation):
     """
--- tests/test_snpeff.py
+++ tests/test_snpeff.py
@@ -7,10 +7,15 @@
 import logging
 import unittest
 from bioformats.snpeff import parse_snpeff_annotation, Record
+from bioformats.snpeff import parse_hgvs_dna, HgvsRecord
 from bioformats.exception import SnpEffError
 
 
 class TestParseSnpEffAnnotation(unittest.TestCase):
+    def setUp(self):
+        # disable logging messages
+        logging.disable(logging.ERROR)
+
     def test_parse_snpeff_annotation(self):
         line = 'ANN=C|missense_variant|MODERATE|OR4F5|' \
                'ENSG00000186092|transcript|ENST00000335137|' \
@@ -23,3 +28,19 @@ class TestParseSnpEffAnnotation(unittest.TestCase):
 
         with self.assertRaises(SnpEffError):
             parse_snpeff_annotation(line[:-50])
+
+    def test_parse_hgvs_dna(self):
+        line = 'c.718G>C'
+        self.assertIsInstance(parse_hgvs_dna(line), HgvsRecord)
+
+        with self.assertRaises(SnpEffError):
+            line = '718G>C'
+            parse_hgvs_dna(line)
+
+        with self.assertRaises(SnpEffError):
+            line = 'c.718GC'
+            parse_hgvs_dna(line)
+            
+        with self.assertRaises(SnpEffError):
+            line = 'c.7G8G>C'
+            parse_hgvs_dna(line)
